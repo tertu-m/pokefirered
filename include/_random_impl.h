2969bf0f249934ee79ca59352baff1bec1eb9f33
tertu marybig <flameshadowxeroshin@gmail.com>
2022-12-22 12:50:50 -0600

    further performance optimizations?
    Port performance optimizations to sfc-vanilla
    It doesn't use them, but if you want to it's a good idea.

--- include/_random_impl.h
+++ include/_random_impl.h
@@ -4,6 +4,31 @@
 #error _random_impl.h was included incorrectly.
 #endif
 
+RANDOM_IMPL_NONCONST u32 _Random32_Unlocked(void) {
+    u32 b, c, result;
+
+    b = gRngState.b;
+    c = gRngState.c;
+    result = gRngState.a + b + gRngState.counter++;
+
+    gRngState.a = b ^ (b >> 9);
+    gRngState.b = (c << 3) + 1;
+    gRngState.c = ((c << 21) | (c >> 11)) + result;
+
+    return result;
+}
+
+
+RANDOM_IMPL_NONCONST u32 Random32(void) {
+    u32 result;
+
+    _LockRng();
+    result = _Random32_Unlocked();
+    _UnlockRng();
+
+    return result;
+}
+
 RANDOM_IMPL_CONST const u16 CountLeadingZeroes(const u32 value)
 {
     u32 modified_value;
@@ -19,35 +44,6 @@ RANDOM_IMPL_CONST const u16 CountLeadingZeroes(const u32 value)
 
     return clz_Lookup[(modified_value * 0x07C4ACDDU) >> 27];
 }
-
-
-RANDOM_IMPL_NONCONST u16 RandomRangeGood(const u16 range)
-{
-    u32 mask, candidate;
-    u16 adjusted_range;
-
-    if (range == 0)
-        return 0;
-
-    adjusted_range = range - 1;
-    mask = ~0U;
-    mask >>= CountLeadingZeroes((u32)range);
-    do {
-        candidate = Random32() & mask;
-    } while (candidate > adjusted_range);
-
-    return candidate;
-}
-
-RANDOM_IMPL_NONCONST u16 RandomPercentageGood() {
-    return RandomRangeGood(100);
-}
-
-//Returns a 16-bit pseudorandom number
-RANDOM_IMPL_NONCONST u16 Random(void) {
-    return RandomBits(16);
-}
-
 
 #undef RANDOM_IMPL_CONST
 #undef RANDOM_IMPL_NONCONST
